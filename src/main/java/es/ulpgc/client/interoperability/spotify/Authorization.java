0c861951d821b172a1547d4fa89c97b4924f94a9
Víctor Medina <90764284+victormedinaa@users.noreply.github.com>
2025-03-20 11:07:48 +0000

    Update Authorization.java

--- src/main/java/es/ulpgc/client/interoperability/spotify/Authorization.java
+++ src/main/java/es/ulpgc/client/interoperability/spotify/Authorization.java
@@ -4,8 +4,8 @@ import es.ulpgc.client.serialization.Base64;
 
 public class Authorization {
 
-    public static final String CLIENT_ID = "9bf63676f0354ecfb8d5bfc4581cb471";
-    public static final String CLIENT_SECRET = "3b444ebbe28f40d4af518078bbc50689";
+    public static final String CLIENT_ID = "your_client";
+    public static final String CLIENT_SECRET = "your_secret";
 
     public static String get() {
         return Base64.encode(CLIENT_ID+":"+ CLIENT_SECRET);
